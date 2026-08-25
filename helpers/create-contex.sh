cb708cbf0ec23577282b5a0c54ec19f3fb215989
TakashiMaruyama-ME <tac_m@hotmail.com>
2026-08-25 10:32:44 -0700

    Add script to create a new Kubernetes context for a given namespace

---
+++ helpers/create-contex.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#create a new context for the given namespace and set it as the current context
+
+NAMESPACE=$1
+
+kubectl config set-context ${NAMESPACE} \
+        --cluster=default \
+        --user=default \
+        --namespace=${NAMESPACE}
+
+kubectl config use-context ${NAMESPACE}
