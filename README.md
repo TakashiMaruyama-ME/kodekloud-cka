86a239cc4af53ba3be960f4f9786bcbe79228617
TakashiMaruyama-ME <tac_m@hotmail.com>
2026-08-27 12:15:38 -0700

    Add Kubernetes installation and configuration scripts for cluster setup

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 Scripts created for solution to https://learn.kodekloud.com/user/courses/cka-certification-course-certified-kubernetes-administrator/module/357c2670-c16c-49ac-aa27-8af52523afde/lesson/a4f095ef-ca2b-4cf9-9c6d-9c6739c3ca6e
-* install.sh
+
+Question 1:
+1. Apply the necessary sysctl parameters for networking.
+  * 1-sysctl.sh
+1. Install kubeadm, kubelet 1.35.0-1.1 on both nodes
+  * 2-install-all.sh
+1. Install kubectl 1.35.0-1.1 on controlplane
+  * 3-install-controlplane.sh
+
 * kube-init.sh
 * kube-config.sh
 * kube-flannel.yml
---
+++ lab-deploy-cluster-w-kubeadm/3-install-node.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+
+# If the directory `/etc/apt/keyrings` does not exist, it should be created before the curl command, read the note below.
+# sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.35/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.35/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+K_VERSION=1.35.0-1.1
+sudo apt-get update
+sudo apt-get install -y kubelet=${K_VERSION} kubeadm=${K_VERSION}
+sudo apt-mark hold kubelet kubeadm
+
+sudo systemctl enable --now kubelet
